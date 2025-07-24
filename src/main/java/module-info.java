ab03f42b8b6610c3a060edfc7695d823991bd3fb
Quang Nam <quangnamnmqo12345@gmail.com>
2025-07-24 17:06:18 +0700

    create new base code

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.quanlynhansu {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.quanlynhansu to javafx.fxml;
+    exports com.example.quanlynhansu;
+}
